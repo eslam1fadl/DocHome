08727b6c95abf1442193aa1325a1c2c4e3c82306
eslam fadl <152933294+eslam1fadl@users.noreply.github.com>
2024-11-28 02:17:17 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var productName=document.getElementById('productName');
+var productPrice=document.getElementById('productPrice');
+var productImage=document.getElementById('productImage');
+var productCat=document.getElementById('productCat');
+var productDec=document.getElementById('productDec');
+
+function addProduct() {
+    
+}
